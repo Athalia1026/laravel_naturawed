d4ad9f5587e4d2ae684dc5ce935ed1976d2673ee
arensha <magrithalucy@gmail.com>
2026-05-24 03:16:09 +0700

    tambah controller buat jurnalis

---
+++ app/Http/Controllers/Journalist/ArticleController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Journalist;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+
+class ArticleController extends Controller
+{
+    // Untuk Jurnalis
+    public function dashboard() {
+        $myArticles = Article::where('user_id', Auth::id())->get();
+        return view('journalist.dashboard', compact('myArticles'));
+    }
+
+    public function create() {
+        return view('journalist.write_article');
+    }
+
+    // Untuk Detail Artikel (Shared oleh Jurnalis & Customer)
+    public function show($id) {
+        $article = Article::findOrFail($id);
+        return view('articles.article_detail', compact('article'));
+    }
+}
